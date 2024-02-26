0367acac6a90fb98d4f62ddbe5c0bf9b755460db
JarlinFonseca <jarlinandres5000@gmail.com>
2024-02-26 12:30:40 -0500

    Se realizo el cambio de la contraseña del administrador

--- parqueadero-service/src/main/java/com/nelumbo/parqueadero/ParqueaderoServiceApplication.java
+++ parqueadero-service/src/main/java/com/nelumbo/parqueadero/ParqueaderoServiceApplication.java
@@ -1,16 +1,27 @@
 package com.nelumbo.parqueadero;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableFeignClients
 @SpringBootApplication
-public class ParqueaderoServiceApplication {
+public class ParqueaderoServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParqueaderoServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String password = "admin";
+		System.out.println(passwordEncoder.encode(password));
+	}
 }
